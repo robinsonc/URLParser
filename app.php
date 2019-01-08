ac57b80daced38aa2fb9c72bceaa5f7b7fa1b67f
Robinson C <robin@wizroots.com>
2019-01-08 21:12:57 +0530

    Added DOM elements of the URL into a file for faster processing

--- app.php
+++ app.php
@@ -14,7 +14,23 @@
 // Check the script is run from Command line
 if (php_sapi_name() == "cli") {
     //In cli-mode
-    echo "CLI mode";
+    try {
+
+		$dom = new DOMDocument; //Create a new DOM document
+		libxml_use_internal_errors(true);
+		//$dom->loadHTMLFile('https://journals.sagepub.com/home/VRT');
+		$dom->loadHTMLFile('journels.html'); //Copied the DOM of https://journals.sagepub.com/home/VRT to a file journels.html
+		$links = $dom->getElementsByTagName('a'); // Get all links.
+
+		foreach ($links as $link){
+			if($link->nodeValue === 'Submit Now') {//Check Node value is same as Submit Now
+				echo $link->getAttribute('href'), PHP_EOL; //Print the Link
+			}
+		}
+	} catch(Exception $e) { //catch exception
+	  echo 'Message: ' .$e->getMessage();
+	}
+    
 } else {
     // Not in cli-mode
 	die('CLI only');	
